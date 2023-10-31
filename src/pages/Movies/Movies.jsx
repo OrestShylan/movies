12c3993d94f82332e18bacfeef40a3135c9cfed3
OrestShylan <orestshylan1997@gmail.com>
2023-10-31 21:31:08 +0200

    fixed infinity fetch on Movies component

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,56 +1,45 @@
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useState} from 'react';
+// import { useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import * as API from '../../api/api-service';
 
 import { SearchForm } from 'components/SearchForm/SearchForm';
-import { Loader } from 'components/Loader/Loader';
 import { MoviesList } from 'components/MoviesList/MoviesList';
 import { Main } from '../Home/Home.styled';
 
-
-
 export default function Movies() {
   const [moviesByName, setMoviesByName] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams();
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSearchForm = searchValue => {
-    setSearchParams({ query: searchValue });
-  };
-
-  useEffect(() => {
-    const value = searchParams.get('query');
-    if (!value) {
-      setMoviesByName([]); // Очистити список фільмів, якщо `query` порожній
+  const handleSearchForm = async searchValue => {
+    if (searchValue.trim() === '') {
+      toast.warn('Sorry, you need to enter a movie title.');
       return;
     }
 
-    if (!isLoading) {
+    try {
       setIsLoading(true);
-
-      API.fetchMoviesByName(value)
-        .then(({ data }) => {
-          if (data && data.results) {
-            setMoviesByName(data.results);
-          } else {
-            setMoviesByName([]); // Очистити список фільмів, якщо результат порожній
-          }
-          setIsLoading(false);
-        })
-        .catch(error => {
-          setIsLoading(false);
-          toast.error(error.message);
-        });
+      const response = await API.fetchMoviesByName(searchValue);
+      const data = response.data;
+      if (data && data.results) {
+        setMoviesByName(data.results);
+      } else {
+        setMoviesByName([]);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
     }
-  }, [searchParams, isLoading]);
+  };
 
   return (
     <Main>
       <SearchForm onSubmit={handleSearchForm} />
-      {isLoading && <Loader />}
-      {moviesByName && moviesByName.length > 0 ? ( // Перевірити, чи moviesByName не порожній
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : moviesByName && moviesByName.length > 0 ? (
         <MoviesList movies={moviesByName} />
       ) : (
         <p>No movies found.</p>
@@ -58,3 +47,62 @@ export default function Movies() {
     </Main>
   );
 }
+
+// import { useState, useEffect } from 'react';
+// import { useSearchParams } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+
+// import * as API from '../../api/api-service';
+
+// import { SearchForm } from 'components/SearchForm/SearchForm';
+// // import { Loader } from 'components/Loader/Loader';
+// import { MoviesList } from 'components/MoviesList/MoviesList';
+// import { Main } from '../Home/Home.styled';
+
+// export default function Movies() {
+//   const [moviesByName, setMoviesByName] = useState([]);
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const handleSearchForm = searchValue => {
+//     setSearchParams({ query: searchValue });
+//   };
+
+//   useEffect(() => {
+//     const value = searchParams.get('query');
+//     if (!value) {
+//       setMoviesByName([]); // Очистити список фільмів, якщо `query` порожній
+//       return;
+//     }
+
+//     if (!isLoading) {
+//       setIsLoading(true);
+
+//       API.fetchMoviesByName(value)
+//         .then(({ data }) => {
+//           if (data && data.results) {
+//             setMoviesByName(data.results);
+//           } else {
+//             setMoviesByName([]); // Очистити список фільмів, якщо результат порожній
+//           }
+//           setIsLoading(false);
+//         })
+//         .catch(error => {
+//           setIsLoading(false);
+//           toast.error(error.message);
+//         });
+//     }
+//   }, [searchParams, isLoading]);
+
+//   return (
+//     <Main>
+//       <SearchForm onSubmit={handleSearchForm} />
+//       {/* {isLoading && <Loader />} */}
+//       {moviesByName && moviesByName.length > 0 ? ( // Перевірити, чи moviesByName не порожній
+//         <MoviesList movies={moviesByName} />
+//       ) : (
+//         <p>No movies found.</p>
+//       )}
+//     </Main>
+//   );
+// }
